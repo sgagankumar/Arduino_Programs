00f8e3d488a8cfb6b4c97c3d043c803f4c210622
Gagan Kumar <31625778+sgagankumar@users.noreply.github.com>
2018-07-22 20:41:24 +0530

    Add files via upload

---
+++ PIR.ino
@@ -0,0 +1,22 @@
+int Status = 0;  // Digital pin D3
+
+int sensor = 2;  // Digital pin D4
+void setup() {
+
+  pinMode(sensor, INPUT);   // declare sensor as input
+  pinMode(Status, OUTPUT);  // declare LED as output
+}
+void loop() {
+
+  long state = digitalRead(sensor);
+    if(state == HIGH) {
+      digitalWrite (Status, HIGH);
+      Serial.println("Motion detected!");
+      delay(1000);
+    }
+    else {
+      digitalWrite (Status, LOW);
+      Serial.println("Motion absent!");
+      delay(1000);
+      }
+}
---
+++ blynkapp.ino
@@ -0,0 +1,57 @@
+#include <Adafruit_Sensor.h>
+#define BLYNK_PRINT Serial
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <Wire.h>
+
+
+
+
+
+char auth[]="c4a28467496c4f7d9ffb40cd9ccf76d3";
+char ssid[] = "vinay";
+char pass[] = "vinay1234";
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE); //Defining the pin and the dhttype
+BlynkTimer timer;
+void sendSensor()
+{
+
+
+
+float h = dht.readHumidity();
+float t = dht.readTemperature();
+  
+
+0  if (isnan(h) || isnan(t))
+  {
+  Serial.println("Failed to read from DHT sensor!");
+  return;
+  }
+
+ 
+Blynk.virtualWrite(V5 , h);
+Blynk.virtualWrite(V6 , t);
+
+
+
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+ 
+  delay(10);
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
